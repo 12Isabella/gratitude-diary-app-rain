6081e2f54c059c1eadd1648e652df14f4efc4a06
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-22 16:37:58 +0200

    add fontawsome for icon, add styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,27 +23,39 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="mt-3 mb-5">What are you grateful for today?</h1>
-      <div className="input-group mb-3 gratitude-input ">
-        <div className="input-group-prepend "></div>
-        <form id="input-form" onSubmit={handleSubmit}>
-          <input
-            value={currentItem}
-            placeholder="I am happy and grateful for..."
-            type="text"
-            className="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            autoFocus="on"
-            onChange={handleChange}
-          />
-        </form>
+      <div className="container mt-3">
+        <h1 className="mt-3 mb-5">What are you grateful for today?</h1>
+        <div className="input-group mb-5 gratitude-input ">
+          <div className="input-group-prepend "></div>
+          <form id="input-form" onSubmit={handleSubmit}>
+            <input
+              value={currentItem}
+              placeholder="I am happy and grateful for..."
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              autoFocus="on"
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <div className="gratitude-item-list">
+          {items.map(function (item, index) {
+            return (
+              <div key={index}>
+                <div className="gratitude-item">
+                  <div>{item}</div>
+                  <footer className="note-footer">
+                    <small>22/09/2021</small>
+                    <i className="fas fa-trash-alt"></i>
+                  </footer>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <ul>
-        {items.map(function (item, index) {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
     </div>
   );
 }
