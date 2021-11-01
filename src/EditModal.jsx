ef0fce269180c46b47b832b7ec3baef67c8b743b
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-11-01 11:48:14 +0100

    add "save edit" icon, add Eventlistener, get edit Input and id

--- src/EditModal.jsx
+++ src/EditModal.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import "./EditModal.css";
+import { useState } from "react";
 
-const EditModal = () => {
+const EditModal = (props) => {
+  const [editInput, setEditInput] = useState(null);
+
+  function handleChange(event) {
+    setEditInput(event.target.value);
+  }
+
+  function saveEdit(event, id) {
+    event.preventDefault();
+    // set EditInput in local storage
+    console.log(id);
+    //Items + updated Item...???
+    // 1. How do I get Items form App.js?
+    // 2. How do I get the edited Item to be edited?
+    //handleEdit (id)
+    // set Edit Input as content of GratitudeItem
+  }
+
   return (
     <div className="editModal">
       <div className="form-group">
-        <label for="exampleFormControlTextarea1"></label>
+        <label htmlFor="exampleFormControlTextarea1"></label>
         <textarea
+          onChange={handleChange}
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
         ></textarea>
+        <i
+          className="fas fa-save icon"
+          onClick={(event) => saveEdit(event, props.id)}
+        ></i>
       </div>
     </div>
   );
